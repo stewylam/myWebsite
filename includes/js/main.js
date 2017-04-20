394a78aa4f047353241467ae6a1f6eddbf50a599
Stephanie Lam <stephaniewylam@hotmail.com>
2017-04-20 22:40:14 +0200

    added jquery scroll styling

--- includes/js/main.js
+++ includes/js/main.js
@@ -17,12 +17,80 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For IE and Firefox
 }
 
+// smooth scrolling
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
+
+// jumbotron slide down
+$(document).ready(function() {
+    $('.jumbotron').hide();
+    $('.jumbotron').slideDown('slow');
+});
+
+// name + buttons will fade in after page loads
+$(document).ready(function() {
+    $("h2").hide();
+    $("h2").fadeIn(1300, function() {
+        
+    });
+})
+
+$(document).ready(function() {
+    $(".btn-toolbar").hide();
+    $(".btn-toolbar").fadeIn(1300, function() {
+        
+    });
+})
+
+
+//profile pic
+$(document).ready(function() {
+    $("#profile").hide();
+    $("#profile").fadeIn(1300, function() {  
+    });
+    $(".col-md-6").hide();
+    $(".col-md-6").fadeIn(1000, function() {  
+    });
+})
+
+
+// fadeIn click menu
+$(document).ready(function() {
+    $(".btn.btn-outline-primary").click( function() {
+        $(".col-md-4, .col-md-12, .photo, .portfolio").hide();
+        $(".col-md-4, .col-md-12, .photo, .portfolio").fadeIn(1800, function() { 
+        });
+});
+});
+
+// alertbox button
 function myFunction() {
     alert("Coming Soon!");
 }
-
-
 
 
 /*portfolio button
